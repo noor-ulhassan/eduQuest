69419deef45ad526857ae7d6c16cf5728b012bef
Arisha-Akbar <arishaakbar713@gmail.com>
2025-11-26 16:04:18 +0500

    Profile Page Edit

--- client/src/layout/MainLayout.jsx
+++ client/src/layout/MainLayout.jsx
@@ -1,5 +1,6 @@
 import Navbar from "@/components/Navbar";
 import React from "react";
+import Footer from "@/components/Footer";
 import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
@@ -9,6 +10,7 @@ const MainLayout = () => {
       <div>
         <Outlet />
       </div>
+      <Footer />
     </div>
   );
 };
