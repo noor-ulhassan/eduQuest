37e955abb9dd69dd869f29b122189cd23e835d0d
Arisha-Akbar <arishaakbar713@gmail.com>
2025-12-14 22:43:01 +0500

    My progress on UserStatus dynamic card

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -12,7 +12,11 @@ const userSchema = new mongoose.Schema({
     bannerUrl: { type: String },
   provider: { type: String, enum: ["local", "google"], default: "local" },
 
-  refreshToken: { type: String, default: null }
+  refreshToken: { type: String, default: null },
+  totalRewards: { type: Number, default: 0 },
+  dailyStreak: { type: Number, default: 0 },
+  badges: { type: Number, default: 0 },
+  level: { type: Number, default: 1 }
 }, { timestamps: true });
 
 export const User = mongoose.model("User", userSchema);
