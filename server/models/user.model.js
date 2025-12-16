149583ecbb8c98818c61cf190572f345d8a0a661
Arisha-Akbar <arishaakbar713@gmail.com>
2025-12-16 21:52:57 +0500

    Revert "My progress on UserStatus dynamic card"
    This reverts commit d6f1e623a58b0062a20a5c1c9d56281a606dc4f9.

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -12,11 +12,7 @@ const userSchema = new mongoose.Schema({
     bannerUrl: { type: String },
   provider: { type: String, enum: ["local", "google"], default: "local" },
 
-  refreshToken: { type: String, default: null },
-  totalRewards: { type: Number, default: 0 },
-  dailyStreak: { type: Number, default: 0 },
-  badges: { type: Number, default: 0 },
-  level: { type: Number, default: 1 }
+  refreshToken: { type: String, default: null }
 }, { timestamps: true });
 
 export const User = mongoose.model("User", userSchema);
