ddfd62eb6688b2c80d142d4990d2fe20136bb30c
Arisha-Akbar <arishaakbar713@gmail.com>
2026-05-03 00:10:08 +0500

    added dark theme in some components

--- client/src/pages/Workspace/EditCourse.jsx
+++ client/src/pages/Workspace/EditCourse.jsx
@@ -7,38 +7,38 @@ import ChapterTopicList from "./components/ChapterTopicList";
 import api from "@/features/auth/authApi";
 
 function EditCourse() {
- const { courseId } = useParams();
- const [loading, setLoading] = useState(false);
- const [course, setCourse] = useState();
+  const { courseId } = useParams();
+  const [loading, setLoading] = useState(false);
+  const [course, setCourse] = useState();
 
- useEffect(() => {
-  if (courseId) {
-   GetCourseInfo();
-  }
- }, [courseId]);
+  useEffect(() => {
+    if (courseId) {
+      GetCourseInfo();
+    }
+  }, [courseId]);
 
- const GetCourseInfo = async () => {
-  setLoading(true);
-  try {
-   console.log("Fetching course with ID:", courseId);
-   const result = await api.get(
-    `http://localhost:8080/api/v1/ai/get-course/${courseId}`
-   );
-   setCourse(result.data.course);
-   console.log("Course fetched successfully:", result.data.course);
-  } catch (error) {
-   console.error("Error fetching course:", error);
-  } finally {
-   setLoading(false);
-  }
- };
+  const GetCourseInfo = async () => {
+    setLoading(true);
+    try {
+      console.log("Fetching course with ID:", courseId);
+      const result = await api.get(
+        `http://localhost:8080/api/v1/ai/get-course/${courseId}`,
+      );
+      setCourse(result.data.course);
+      console.log("Course fetched successfully:", result.data.course);
+    } catch (error) {
+      console.error("Error fetching course:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
- return (
-  <div className="mt-20 px-10">
-   <CourseInfo course={course} />
-   <ChapterTopicList course={course} />
-  </div>
- );
+  return (
+    <div className="px-10 bg-[#0A0A0B] min-h-screen pt-24 pb-10">
+      <CourseInfo course={course} />
+      <ChapterTopicList course={course} />
+    </div>
+  );
 }
 
 export default EditCourse;
